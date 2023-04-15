485fd389ee408e55fed07aa5241cc387aafc7042
Admin <syubebayevm@gmail.com>
2023-04-16 03:15:54 +0500

    created static html elements

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 'use strict'
 
-
+// creating an array
 let array = [
-    
+    {
+        author: "Gulien",
+        topic_name: "cats",
+        published_date: "5 March",
+        title: "The most beautiful cats",
+        short_desc: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+        theme: "Animals",
+        time_over: "15 min",
+        select: "selected for you",
+        image: "images/0.png"
+
+    },
+    {
+        author: "Myiosh",
+        topic_name: "dogs",
+        published_date: "7 February",
+        title: "The most beautiful dogs",
+        short_desc: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+        theme: "hunters",
+        time_over: "25 min",
+        select: "selected for miosh",
+        image: "images/1.png"
+
+    },
+    {
+        author: "Relon",
+        topic_name: "snakes",
+        published_date: "8 May",
+        title: "The most beautiful snakes",
+        short_desc: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+        theme: "Wild Animals",
+        time_over: "35 min",
+        select: "selected for relon",
+        image: "images/2.png"
+
+    }
 ]
+
+
+
