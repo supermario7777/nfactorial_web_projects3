d05e0ee022e8e16af5a495d82787c5af662bbb18
Admin <syubebayevm@gmail.com>
2023-04-16 16:33:32 +0500

    first half of the project

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-'use strict'
+// 'use strict'
 
 // creating an array
-let array = [
+let articles = [
     {
         author: "Gulien",
         topic_name: "cats",
@@ -32,18 +32,43 @@ let array = [
         published_date: "8 May",
         title: "The most beautiful snakes",
         short_desc: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-        theme: "Wild Animals",
+        theme: "Animals",
         time_over: "35 min",
         select: "selected for relon",
         image: "images/2.png"
 
-    }
-]
+    },
+];
 
-const articlesDiv = document.getElementsByClassName("articles")
-console.log(articlesDiv)
-array.map((item)=>{
-    const html = ``
+const newsSection = document.getElementById("news")
+console.log(newsSection)
+articles.map((item) => {
+    const html = `
+        <div class="articles">
+            <div class="header">
+                <div class="top">
+                    <p class="author">${item.author}</p>
+                    <p class="in">in</p>
+                    <p class="topic_name">${item.topic_name}</p>
+                    <p class="published_date">${item.published_date}</p>
+                </div>
+                <div class="middle">                    
+                    <h2 class="title">${item.title}</h2>
+                    <p class="short_desc">${item.short_desc}</p>
+                </div>
+                <div class="bottom">
+                    <button class="theme">${item.theme}</button>                
+                    <p class="time_over">${item.time_over}</p>
+                    <li class="select">${item.select}</li>
+                </div>
+            </div>
+            <div class="images">
+                <img src="images/0.png" alt="image0">
+            </div>
+            <div class="border"></div>
+        </div>`;       
+
+        newsSection.innerHTML += html;
 })
 
 
